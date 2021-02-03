524b8b2e6a39215fb571e606e3b8524af2e80c92
홍종완 <infinity4869@gmail.com>
2021-02-03 15:45:44 +0900

    change directory

---
+++ base/dictionary.py
@@ -0,0 +1,25 @@
+# 딕셔너리 - key, value의 쌍을 데이터로 가지는 자료형
+data = dict()
+data['사과'] = 'Apple'
+data['바나나'] = 'Banana'
+data['코코넛'] = 'Coconut'
+
+print(data)
+
+# 파이썬의 딕셔너리 자료형은 내부적으로 '해시 테이블'을 이용하므로 데이터의 검색 및 수정에 있어서 O(1)의 시간에 처리 가능
+
+# 딕셔너리에 특정한 원소가 있는지 검사할 때는 '원소 in 사전'의 형태를 사용할 수 있다.
+if '사과' in data:
+    print("'사과'를 키로 가지는 데이터가 존재")
+
+# 딕셔너리와 관련된 함수
+
+# 키 데이터만 담은 리스트
+key_list = data.keys()
+# 값 데이터만 담은 리스트
+value_list = data.values()
+print(key_list)
+print(value_list)
+# 각 키에 따른 값을 하나씩 출력
+for key in key_list:
+    print(data[key])
