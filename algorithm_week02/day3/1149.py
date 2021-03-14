f1de1436e094658f9261ba4c9ec05d1c71bfe31d
jongwanhong <infinity4869@gmail.com>
2021-03-14 20:55:40 +0900

    add fibo dp bottom up

--- algorithm_week02/day3/1149.py
+++ algorithm_week02/day3/1149.py
@@ -1,17 +1,20 @@
 import sys
 
 n = int(input())
-color = map(int, sys.stdin.readline())
 
-
-# def house(i, j):
-
-#     if dp[j][i] == min(dp[i]):
-
-#         return dp[j][i]
-
-#     return dp[j][i]
-
-
-# dp = [[0 for _ in range(3)] for _ in range(n)]
+dp = [[0 for _ in range(3)] for _ in range(n)]
 # print(dp)
+
+for i in range(n):
+    dp[i] = list(map(int, sys.stdin.readline().split()))
+    dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + dp[i][0]
+    # i번째 집을 빨간색으로 칠했을 때는 i-1 집이 초록,파랑에서 최솟값으로 칠해야 하고,
+    # 최솟값과 빨간색칠 한 값을 더함
+    dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + dp[i][1]
+    # i번째 집을 초록색으로 칠했을 때는 i-1 집이 빨강,파랑에서 최솟값으로 칠해야 하고,
+    # 최솟값과 초록색칠 한 값을 더함
+    dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + dp[i][2]
+    # i번째 집을 파랑색으로 칠했을 때는 i-1 집이 빨강,초록에서 최솟값으로 칠해야 히고,
+    # 최솟값과 파랑색칠 한 값을 더함
+
+print(min(dp[n - 1][0], dp[n - 1][1], dp[n - 1][2]))
