63cdbfc42765806804d966a813a31dfeffd7c897
wanjongth <53491653+wanjongth@users.noreply.github.com>
2021-06-06 23:07:58 +0900

    fixed for test case but time out

--- programmers/study/price.py
+++ programmers/study/price.py
@@ -1,6 +1,19 @@
 # 초 단위로 기록된 주식가격이 담긴 배열 prices가 매개변수로 주어질 때,
 # 가격이 떨어지지 않은 기간은 몇 초인지를 return 하도록 solution 함수를 완성하세요.
 
+# def solution(prices):
+#     answer = []
+
+#     for i in range(len(prices)):
+#         cnt = 0
+#         for j in prices[i:]:
+#             if prices[i] <= j:
+#                 cnt += 1
+
+#         answer.append(cnt-1)
+
+#     return answer
+
 def solution(prices):
     answer = []
 
@@ -9,6 +22,9 @@ def solution(prices):
         for j in prices[i:]:
             if prices[i] <= j:
                 cnt += 1
+            else:
+                cnt += 1
+                break
 
         answer.append(cnt-1)
 
