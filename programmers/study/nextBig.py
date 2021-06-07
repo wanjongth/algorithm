29fc75a78954d8a6bf3a551c6d343eb29c0f4ad2
wanjongth <53491653+wanjongth@users.noreply.github.com>
2021-06-07 17:24:03 +0900

    0609 study prob

---
+++ programmers/study/nextBig.py
@@ -0,0 +1,46 @@
+# 자연수 n이 주어졌을 때, n의 다음 큰 숫자는 다음과 같이 정의 합니다.
+
+# 조건 1. n의 다음 큰 숫자는 n보다 큰 자연수 입니다.
+# 조건 2. n의 다음 큰 숫자와 n은 2진수로 변환했을 때 1의 갯수가 같습니다.
+# 조건 3. n의 다음 큰 숫자는 조건 1, 2를 만족하는 수 중 가장 작은 수 입니다.
+# 예를 들어서 78(1001110)의 다음 큰 숫자는 83(1010011)입니다.
+
+# 자연수 n이 매개변수로 주어질 때, n의 다음 큰 숫자를 return 하는 solution 함수를 완성해주세요.
+
+# 진법 변환하기(2~16) - 인터넷에서 주워옴..
+NOTATION = '0123456789ABCDEF'
+
+
+def numeral_system(number, base):
+    q, r = divmod(number, base)
+    n = NOTATION[r]
+    return numeral_system(q, base) + n if q else n
+
+# # 패턴 찾기 -> 실패
+
+
+# def test(n):
+#     answer = numeral_system(n, 2)
+#     return answer
+
+
+# for i in range(1, 21):
+#     print(str(i)+'는 '+test(i))
+
+# 본 코드
+
+
+def solution(n):
+    answer = 0
+    one_cnt = numeral_system(n, 2).count('1')
+    for i in range(n+1, 1000001):  # 범위는 2n+1까지만 잡아줘도 된다고 함 -> break문 때문에 상관없긴 할듯
+        if one_cnt == numeral_system(i, 2).count('1'):
+            answer = i
+            break
+    return answer
+
+
+print(solution(15))
+print(solution(78))
+
+# 다른사람 풀이보니 bin이라는 함수가 있었음 -> 2진법 변환
--- programmers/study/price.py
+++ programmers/study/price.py
@@ -14,20 +14,38 @@
 
 #     return answer
 
+# # 시간초과
+# def solution(prices):
+#     answer = []
+
+#     for i in range(len(prices)):
+#         cnt = 0
+#         for j in prices[i:]:
+#             if prices[i] <= j:
+#                 cnt += 1
+#             else:
+#                 cnt += 1
+#                 break
+
+#         answer.append(cnt-1)
+
+#     return answer
+
+# 스택 사용 -> 값이 아닌 가격의 인덱스를 스택에 넣었다가 뻄
 def solution(prices):
-    answer = []
-
-    for i in range(len(prices)):
-        cnt = 0
-        for j in prices[i:]:
-            if prices[i] <= j:
-                cnt += 1
-            else:
-                cnt += 1
-                break
-
-        answer.append(cnt-1)
-
+    answer = [0] * len(prices)
+    stack = [0]
+    for i in range(1, len(prices)):
+        if prices[i] < prices[stack[-1]]:
+            for j in stack[::-1]:  # 증가폭 -1 -> 새로 알게 됨
+                if prices[i] < prices[j]:
+                    answer[j] = i - j
+                    stack.remove(j)
+                else:
+                    break
+        stack.append(i)
+    for i in range(0, len(stack)-1):
+        answer[stack[i]] = len(prices) - stack[i] - 1
     return answer
 
 
